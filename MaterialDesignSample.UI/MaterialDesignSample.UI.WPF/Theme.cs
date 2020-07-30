07e2274182781a953be8a6033ad2092b84eabee7
Raphael Härtel <raphael.haertel@bluewin.ch>
2020-07-30 11:42:23 +0200

    GroupBox Style

--- MaterialDesignSample.UI/MaterialDesignSample.UI.WPF/Theme.cs
+++ MaterialDesignSample.UI/MaterialDesignSample.UI.WPF/Theme.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.Windows;
-using System.Windows.Documents;
-using System.Windows.Media;
 using MaterialDesignSample.UI.WPF.Brushes;
 
 namespace MaterialDesignSample.UI.WPF
