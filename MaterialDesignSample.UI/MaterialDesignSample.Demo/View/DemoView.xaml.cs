bc4bd3db5d01a38523e6115448bb589ae562110d
Raphael Härtel <raphael.haertel@bluewin.ch>
2020-07-30 15:45:48 +0200

    Window Style

--- MaterialDesignSample.UI/MaterialDesignSample.Demo/View/DemoView.xaml.cs
+++ MaterialDesignSample.UI/MaterialDesignSample.Demo/View/DemoView.xaml.cs
@@ -1,22 +1,13 @@
-﻿using System.Windows;
-using MaterialDesignSample.UI.WPF;
-
-namespace MaterialDesignSample.Demo.View
+﻿namespace MaterialDesignSample.Demo.View
 {
     /// <summary>
     /// Interaction logic for DemoView.xaml
     /// </summary>
-    public partial class DemoView : Window
+    public partial class DemoView
     {
         public DemoView()
         {
             InitializeComponent();
-        }
-
-        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
-        {
-            var theme = Theme.ThemeType == ThemeType.Light ? ThemeType.Dark : ThemeType.Light;
-            Theme.LoadThemeType(theme);
         }
     }
 }
---
+++ MaterialDesignSample.UI/MaterialDesignSample.UI.WPF/Themes/Styles/WindowStyles.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+
+namespace MaterialDesignSample.UI.WPF.Themes.Styles
+{
+    public partial class WindowStyles
+    {
+        public WindowStyles()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCloseClick(object sender, RoutedEventArgs e)
+        {
+            var window = (Window)((FrameworkElement)sender).TemplatedParent;
+            window.Close();
+        }
+
+        private void OnMaximzeRestoreClick(object sender, RoutedEventArgs e)
+        {
+            var window = (Window)((FrameworkElement)sender).TemplatedParent;
+            window.WindowState = window.WindowState == WindowState.Normal ? WindowState.Maximized : WindowState.Normal;
+        }
+
+        private void OnMinimizeClick(object sender, RoutedEventArgs e)
+        {
+            var window = (Window)((FrameworkElement)sender).TemplatedParent;
+            window.WindowState = WindowState.Minimized;
+        }
+
+        private void OnThemeClick(object sender, RoutedEventArgs e)
+        {
+            var theme = Theme.ThemeType == ThemeType.Light ? ThemeType.Dark : ThemeType.Light;
+            Theme.LoadThemeType(theme);
+        }
+    }
+}
