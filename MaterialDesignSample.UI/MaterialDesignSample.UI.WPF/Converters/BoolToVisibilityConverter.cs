b8608897f006016c9cd621170f429d7c183e921a
Raphael Härtel <raphael.haertel@bluewin.ch>
2020-08-17 10:32:38 +0200

    BoolToVisiblity Converter

---
+++ MaterialDesignSample.UI/MaterialDesignSample.UI.WPF/Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace MaterialDesignSample.UI.WPF.Converters
+{
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        public bool IsInverse { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is bool))
+                return null;
+
+            return (bool)value ^ IsInverse ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
